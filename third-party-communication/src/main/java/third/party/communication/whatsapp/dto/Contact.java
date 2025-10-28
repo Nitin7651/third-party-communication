eaa0706fb512825e51361dfe760b92e1c67106e1
Nitin Dwivedi <Shivadwivedi7651@gmail.com>
2025-10-28 13:44:50 +0530

    final

--- third-party-communication/src/main/java/third/party/communication/whatsapp/dto/Contact.java
+++ third-party-communication/src/main/java/third/party/communication/whatsapp/dto/Contact.java
@@ -5,13 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor // Default constructor needed for JSON parsing if used in requests
 @AllArgsConstructor // Constructor for creating objects easily
 public class Contact {
     private String name;
-    private String number;
+    private List<String> number;
 
     // Manual Getters/Setters/Constructors if not using Lombok
 }
